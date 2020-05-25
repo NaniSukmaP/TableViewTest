d9c5e507006e3a4f91190f71299a78b47b43dbed
NaniSukmaP <luckyputri98@gmail.com>
2020-05-25 13:07:59 +0700

    Tableview Static

---
+++ TableViewTest/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TableViewTest
+//
+//  Created by Nani Sukma Putri Pratama on 25/05/20.
+//  Copyright © 2020 Nani Sukma Putri Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
